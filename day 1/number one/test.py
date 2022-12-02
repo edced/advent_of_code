8345b9de3db7efe06b363190255677ac35bd1932
edced <cedricdewaerheijd@gmail.com>
2022-12-02 11:56:36 +0100

    Update test.py

--- day 1/number one/test.py
+++ day 1/number one/test.py
@@ -9,10 +9,12 @@ max = 0
 max1 = 0
 max2 = 0
 max3 = 0
+a=0
+serie = 1
 for item in content:
     if item == '':
+
        
-            
         if (calories > max1 and calories > max2 and calories > max3):
             max1 = calories
         if (calories < max1 and calories > max2 and calories > max3):
@@ -25,8 +27,17 @@ for item in content:
     else:
         num = int(item) #creates an int from sting
         calories+= num
+        
 
-print ("max = ", max, '\n')
-print("sum of top 3 max = ", max1 + max2 + max3, '\n')
-print("wrong sum of maxes = ", 204882, '\n')
-print("max*3 = ", 70613*3, '\n')
+if max1 + max2 + max3 == 204882:
+    print("wrong")
+
+else:
+    print("sum of top 3 max = ", max1 + max2 + max3, '\n')
+
+# print("max = ", max, '\n')
+# print("max 1, max2, max3: ", max1 , max2, max3)
+# print("sum of top 3 max = ", max1 + max2 + max3, '\n')
+# print("wrong sum of maxes = ", 204882, '\n')
+# print("max*3 = ", 70613*3, '\n')
+# print(70613+68330+65939)
